622ba7ec35085f0eff7b0f9e27b78bfbf8e95c67
Bastian Widmer <b.widmer@dasrecht.net>
2012-01-08 16:34:46 +0100

    formating and whitespace changes

--- php_webservice/index.php
+++ php_webservice/index.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 /* *
  * PHP Webservice for the Arduino Busstop Light
  *
@@ -8,46 +7,41 @@
  * Just change the $url $walkingtime and $offtime to your needs.
  *
  * */
+include ('simple_html_dom.php');
 
-
-include('simple_html_dom.php');
-
-$walkingtime = 180;	// Define walking Time to the Bus Stop
-$offtime = 300;		// Signal an OFF Event if time is more than 5 minutes
-$url ='http://fahrplan.search.ch/bern,aeussere-enge/bern'; // Get the timetable data url from fahrplan.search.ch
+$walkingtime = 180; // Define walking Time to the Bus Stop
+$offtime = 300; // Signal an OFF Event if time is more than 5 minutes
+$url = 'http://fahrplan.search.ch/bern,aeussere-enge/bern'; // Get the timetable data url from fahrplan.search.ch
 
 $html = file_get_html($url);
 
 // if there's a error returned in our query return error and the message and exit
-if ($html->find('div.sl_warning', 0)){
-	echo "error - " . $html->find('div.sl_warning', 0)->plaintext;
-	exit;
+if ($html->find('div.sl_warning', 0)) {
+    echo "error - ".$html->find('div.sl_warning', 0)->plaintext;
+    exit;
 }
 
 // calculate next busstop
-foreach($html->find('span.oev_printbold') as $e)	//Get the First DIV with StartData
-{
-$nextbus = strtotime(substr(trim($e->innertext),0,5));	// Strip unneccesary data and convert to a timestamp
+foreach ($html->find('span.oev_printbold') as $e) //Get the First DIV with StartData {
+    $nextbus = strtotime(substr(trim($e->innertext), 0, 5)); // Strip unneccesary data and convert to a timestamp
+    $now
+    = strtotime(date("G:i")); // Create a timestamp reflecting the current time
+    $diff
+    = $nextbus - $now; // calculate the difference for the events
+    if
 
-$now = strtotime(date("G:i"));				// Create a timestamp reflecting the current time
-
-$diff = $nextbus - $now;				// calculate the difference for the events
-
-
-if ($diff < $walkingtime) {
-	if ($diff < 0) {continue;}			// if we get a negative value of time (can happen if the server time is not 100% accurate) skip this result and wait for the next bus
-  echo "red - " . $diff;
-}
-  else
-{
-  if ($diff > $offtime) {
-    echo "off - " .$diff;
-  }
-    else
-  {
-    echo "green - ".$diff;
-  }
-}
-echo "\n";
-exit;
+    ($diff < $walkingtime) {
+        if ($diff < 0) {
+            continue;
+        } // if we get a negative value of time (can happen if the server time is not 100% accurate) skip this result and wait for the next bus
+        echo "red - ".$diff;
+    } else {
+        if ($diff > $offtime) {
+            echo "off - ".$diff;
+        } else {
+            echo "green - ".$diff;
+        }
+    }
+    echo "\n";
+    exit;
 }
